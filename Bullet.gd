0f23954d85c3099eb7c1d6953ff4f43358960cfb
srijal30 <srijal30@stuy.edu>
2022-04-05 22:52:06 -0400

    stuff

---
+++ Bullet.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+var speed :int = 700
+var trajectory : Vector2 = Vector2(0, 0)
+
+	
+func _physics_process(delta):
+	
+	if abs(position.x) == 200:
+		queue_free()
+		
+	if abs(position.y) == 200:
+		queue_free()
+	
+	position += transform.x * speed * delta
+
+	
--- Player.gd
+++ Player.gd
@@ -1,24 +1,26 @@
 extends KinematicBody2D
 
 
+#player movement vars
 var speed : int  = 300
 var velocity : Vector2  = Vector2(0, 0)
 var dashMultiplier : int = 10
 
+#store mouse position and create pointer object
 var mousePos : Vector2 = Vector2(0, 0)
+onready var pointer = get_node("Pointer")
 
-var pointer
 
-func _ready():
-	pointer = get_node("Pointer")	
+#store bullet
+onready var Bullet = preload("res://Bullet.tscn")
+
 
 func _physics_process(delta):
-	
 	#reset
 	velocity.x = 0
 	velocity.y = 0
 	
-	#inputs
+	#movement inputs
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= speed
 	if Input.is_action_pressed("move_down"):
@@ -28,6 +30,7 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		velocity.x += speed
 	
+	#for rolling
 	if Input.is_action_just_pressed("roll"):
 		velocity *= dashMultiplier
 		
@@ -44,6 +47,17 @@ func _physics_process(delta):
 	#right direction of pointer
 	if pointer.visible:
 		pointer.set_rotation_degrees( rad2deg( mousePos.angle_to_point(position) ) + 90)
+		
+	#shoot
+	if Input.is_action_just_pressed("attack"):
+		#create instance of new bullet
+		var bullet = Bullet.instance()
+		#add it as child to owner
+		owner.add_child(bullet)
+		#set position of bullet to player position
+		bullet.position = position
+		bullet.set_rotation_degrees( pointer.get_rotation_degrees() - 90)
+			
 
 
 func _input(event):
