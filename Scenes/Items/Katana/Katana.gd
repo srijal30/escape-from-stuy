830214bb019ea13cfe34cfb9892dc280cc5b9e03
srijal30 <srijal30@stuy.edu>
2022-05-12 20:12:28 -0400

    stuff

---
+++ Scenes/Items/Katana/Katana.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func attack():
+	$AnimationPlayer.play("swing") 
--- Scripts/Weapons/Guitar.gd
+++ Scripts/Weapons/Guitar.gd
@@ -14,8 +14,11 @@ func attack():
 	
 #strum (maybe cooldown)
 func second_attack():
+	
 	print("there is no second attack!")
 
+
+#when guitar body hits while swinging
 func _on_AttackArea_area_entered(area):
 	if area.is_in_group("enemy") and $AnimationPlayer.current_animation == "swing":
 		area.get_parent().take_damage( swingDamage )
