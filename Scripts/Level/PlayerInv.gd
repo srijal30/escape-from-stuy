c584ec20138cd4df6913932ff3ad299dfdab9db7
srijal30 <srijal30@stuy.edu>
2022-05-14 19:15:26 -0400

    changed color

--- Scripts/Level/PlayerInv.gd
+++ Scripts/Level/PlayerInv.gd
@@ -1,12 +1,17 @@
 extends Node
+
 var weapons = []
 var passives = []
+
 func add_weapon(addition):
 	weapons.append(addition)
+
 func add_passive(addition):
 	passives.append(addition)
+
 func remove_weapon(remove):
 	weapons.remove(remove)
+
 func remove_passive(remove):
 	passives.remove(remove)
 	
