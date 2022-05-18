1237293eb3545e0f49a2dfd461d194aee0f7bb92
Jeff Chen <cytastorm@gmail.com>
2022-05-17 23:31:08 -0400

    finished katana swing and pickup and swap

--- Scripts/Weapons/Katana.gd
+++ Scripts/Weapons/Katana.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
+export var damage = 70
 
 func attack():
-	$AnimationPlayer.play("swing") 
+	$AnimationPlayer.play("katana_swing") 
 # timer: 2 sec window to perform 2nd attack
 #   2nd animation
 # timer: 2 sec window to perform 3rd attack
@@ -11,3 +12,7 @@ func attack():
 func second_attack():
 	
 	print("there is no second attack!")
+
+func _on_katana_area_entered(area):
+	if area.is_in_group("enemy") and $AnimationPlayer.current_animation == "swing":
+		area.get_parent().take_damage(damage)
